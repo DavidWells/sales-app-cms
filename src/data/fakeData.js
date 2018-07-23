4d747094a60ab90987584affe46ddabdd333ed4f
chen <chen.cohen87@gmail.com>
2018-07-23 10:01:06 +0300

    data for afrodita

--- src/data/fakeData.js
+++ src/data/fakeData.js
@@ -1,37 +1,37 @@
-import image2 from '../assets/2.jpg'
-import image3 from '../assets/3.jpg'
-import image1 from '../assets/1.jpg'
-import image4 from '../assets/4.jpg'
-import image5 from '../assets/5.jpg'
-import image6 from '../assets/6.jpg'
-import image7 from '../assets/7.png'
+// import image2 from '../assets/2.jpg'
+// import image3 from '../assets/3.jpg'
+// import image1 from '../assets/1.jpg'
+// import image4 from '../assets/4.jpg'
+// import image5 from '../assets/5.jpg'
+// import image6 from '../assets/6.jpg'
+// import image7 from '../assets/7.png'
 
 export const dataItems = [
   {
-    title: 'Dress',
+    title: 'חזיית המינימייזר',
     id: '98797',
-    image: image1,
+    image: "https://fashion-media.apps-checkout.com/afrodita/16030_1525327134.jpg",
+    badge: 'bestSeller',
+    badgeTitle: 'Best Seller',
+  },
+  {
+    title: 'גוזייה מרופדת',
+    id: '98797',
+    image: "https://fashion-media.apps-checkout.com/afrodita/17010_1525327134.jpg",
     badge: 'bestSeller',
     badgeTitle: 'Best Seller',
   },
   {
     title: 'Dress',
     id: '98797',
-    image: image1,
+    image: "https://fashion-media.apps-checkout.com/afrodita_98745-v-1509610422.jpg",
     badge: 'bestSeller',
     badgeTitle: 'Best Seller',
   },
   {
     title: 'Dress',
     id: '98797',
-    image: image1,
-    badge: 'bestSeller',
-    badgeTitle: 'Best Seller',
-  },
-  {
-    title: 'Dress',
-    id: '98797',
-    image: image1,
+    image: "https://fashion-media.apps-checkout.com/afrodita/991526_1525327134.jpg",
     badge: 'bestSeller',
     badgeTitle: 'Best Seller',
   },
@@ -39,63 +39,63 @@ export const dataItems = [
   {
     title: 'Swimsuit',
     id: '98797',
-    image: image2,
+    image: "https://fashion-media.apps-checkout.com/afrodita/991527_1525327134.jpg",
     badge: 'trending',
     badgeTitle: 'Trending',
   },
   {
     title: 'Dress',
     id: '98797',
-    image: image6,
+    image: "https://fashion-media.apps-checkout.com/afrodita_03-12c347_-v-1509610422.jpg",
     badge: 'trending',
     badgeTitle: 'Trending',
   },
   {
     title: 'Swimsuit',
     id: '98797',
-    image: image2,
+    image: "https://fashion-media.apps-checkout.com/afrodita_09-12c541-v-1509610422.jpg",
     badge: 'trending',
     badgeTitle: 'Trending',
   },
   {
     title: 'Dress',
     id: '98797',
-    image: image6,
+    image: "https://fashion-media.apps-checkout.com/afrodita_093a1205-v-1509610422.jpg",
     badge: 'trending',
     badgeTitle: 'Trending',
   },
   {
     title: 'Red Dress',
     id: '98797',
-    image: image3,
+    image: "https://fashion-media.apps-checkout.com/afrodita_11180_-v-1509610422.jpg",
     badge: 'new',
     badgeTitle: 'New',
   },
   {
     title: 'Swimsuit',
     id: '98797',
-    image: image7,
+    image: "https://fashion-media.apps-checkout.com/afrodita_12b330_3-v-1509610422.jpg",
     badge: 'new',
     badgeTitle: 'New',
   },
   {
     title: 'Swimsuit',
     id: '98797',
-    image: image7,
+    image: "https://fashion-media.apps-checkout.com/afrodita_12b347_2_-v-1509610422.jpg",
     badge: 'new',
     badgeTitle: 'New',
   },
   {
     title: 'Red Dress',
     id: '98797',
-    image: image3,
+    image: "https://fashion-media.apps-checkout.com/afrodita_131343__6-v-1509610422.jpg",
     badge: 'new',
     badgeTitle: 'New',
   },
   {
     title: 'Blue Dress',
     id: '98797',
-    image: image4,
+    image: "https://fashion-media.apps-checkout.com/afrodita/13135T_1525327134.jpg",
     badge: 'onSale',
     badgeTitle: 'On Sale',
   },
@@ -103,14 +103,14 @@ export const dataItems = [
   {
     title: 'Blue Dress',
     id: '98797',
-    image: image6,
+    image: "https://fashion-media.apps-checkout.com/afrodita_13e330-v-1509610422.jpg",
     badge: 'onSale',
     badgeTitle: 'On Sale',
   },
   {
     title: 'Blue Dress',
     id: '98797',
-    image: image1,
+    image: "https://fashion-media.apps-checkout.com/afrodita/27401T_1525327134.jpg",
     badge: 'onSale',
     badgeTitle: 'On Sale',
   },
@@ -118,7 +118,7 @@ export const dataItems = [
   {
     title: 'Blue Dress',
     id: '98797',
-    image: image2,
+    image: "https://fashion-media.apps-checkout.com/afrodita/3546608289241_1513781146.jpg",
     badge: 'onSale',
     badgeTitle: 'On Sale',
   },
@@ -126,14 +126,14 @@ export const dataItems = [
   {
     title: 'White Dress',
     id: '98797',
-    image: image5,
+    image: "https://fashion-media.apps-checkout.com/afrodita/7297504077390_1513865031.jpg",
     badge: 'missing',
     badgeTitle: 'Missing',
   },
   {
     title: 'Red Dress',
     id: '98797',
-    image: image2,
+    image: "https://fashion-media.apps-checkout.com/afrodita/7297504088754_1513782074.jpg",
     badge: 'missing',
     badgeTitle: 'Missing',
   },
@@ -141,7 +141,7 @@ export const dataItems = [
   {
     title: 'White Dress',
     id: '98797',
-    image: image5,
+    image: "https://fashion-media.apps-checkout.com/afrodita_786155_1-v-1509610422.jpg",
     badge: 'missing',
     badgeTitle: 'Missing',
   },
@@ -149,7 +149,7 @@ export const dataItems = [
   {
     title: 'Red Dress',
     id: '98797',
-    image: image2,
+    image: "https://fashion-media.apps-checkout.com/afrodita_777418-v-1512750034.jpg",
     badge: 'missing',
     badgeTitle: 'Missing',
   },
