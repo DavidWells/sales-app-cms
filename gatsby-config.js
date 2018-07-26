df7bf25e0c2fe74348e8e0a7d8778536e39a1a3d
ruslane@mystore-e.com <ruslane@mystore-e.com>
2018-07-26 12:44:37 +0300

    Manifest fixes and icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
+const config = require('./AppConfig')
+
 module.exports = {
   siteMetadata: {
-    title: 'Mystore-e',
-    description: 'Sales App by Mystore-e',
+    title: 'Kay',
+    description: 'Kay Jewelers',
     siteUrl: 'https://sales-app.netlify.com',
     author: 'mystore-e',
     twitter: 'mystore-e',
@@ -43,9 +45,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Mystore-e',
-        short_name: 'Mystore-e',
-        description: 'Sales App by Mystore-e',
+        name: 'Kay Jewelers',
+        short_name: 'Kay',
+        description: 'Kay Jewelers',
         homepage_url: 'https://sales-app.netlify.com',
         start_url: '/',
         background_color: '#fff',
@@ -54,12 +56,37 @@ module.exports = {
         display: 'fullscreen',
         icons: [
           {
-            src: '/img/icon-192x192.png',
+            src: '/icons/icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/img/icon-512x512.png',
+            src: '/icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
