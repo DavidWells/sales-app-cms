69c896009cbdf372265ad277c4c6f9392d38991a
ruslane@mystore-e.com <ruslane@mystore-e.com>
2018-08-09 12:34:03 +0300

    ...

--- src/components/Elements/Logo.jsx
+++ src/components/Elements/Logo.jsx
@@ -12,7 +12,9 @@ const StyledLogo = styled.img`
 
 const Logo = props => {
   console.log(props.logoImage)
-  return <StyledLogo className="logo" src={props.logoImage} alt="" />
+  return (
+    <StyledLogo className="logo" src={'/img/emblem-ralph-lauren.png'} alt="" />
+  )
 }
 
 Logo.propTypes = {
