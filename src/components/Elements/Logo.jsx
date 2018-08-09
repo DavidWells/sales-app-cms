826b908477d13ae99df6c3770f070ed04109b225
ruslane@mystore-e.com <ruslane@mystore-e.com>
2018-08-09 11:58:00 +0300

    logo fix

--- src/components/Elements/Logo.jsx
+++ src/components/Elements/Logo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import defaultLogo from '../../assets/kay-logo.png'
+// import defaultLogo from '../../assets/kay-logo.png'
 import styled from 'styled-components'
 
 import { connect } from 'unistore/react'
@@ -14,12 +14,13 @@ const Logo = props => {
   return <StyledLogo className="logo" src={props.logo} alt="" />
 }
 
-Logo.defaultProps = {
-  src: defaultLogo,
-  logo: defaultLogo,
-}
+// Logo.defaultProps = {
+//   src: defaultLogo,
+//   logo: defaultLogo,
+// }
 Logo.propTypes = {
-  src: PropTypes.string,
+  // src: PropTypes.string,
+  logo: PropTypes.any,
 }
 
 const mapStateToProps = ({ logo }) => ({
